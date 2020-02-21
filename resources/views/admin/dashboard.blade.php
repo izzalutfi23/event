4780def4e511fe94b40329d90fb155628ea3550a
izza <izzalutfi045@gmail.com>
2020-02-21 11:53:38 +0700

    mengubah pada halaman dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 col-xl-6">
+            <div class="col-md-6 col-xl-4">
                 <div class="card mb-3 widget-content bg-midnight-bloom">
                     <div class="widget-content-wrapper text-white">
                         <div class="widget-content-left">
@@ -31,7 +31,20 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-6">
+            <div class="col-md-6 col-xl-4">
+                <div class="card mb-3 widget-content bg-arielle-smile">
+                    <div class="widget-content-wrapper text-white">
+                        <div class="widget-content-left">
+                            <div class="widget-heading">Admin</div>
+                            <div class="widget-subheading">Jumlah Semua Admin</div>
+                        </div>
+                        <div class="widget-content-right">
+                            <div class="widget-numbers text-white"><span>{{$admin}}</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-4">
                 <div class="card mb-3 widget-content bg-grow-early">
                     <div class="widget-content-wrapper text-white">
                         <div class="widget-content-left">
